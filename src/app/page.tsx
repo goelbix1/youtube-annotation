df0cb6d4b49e5356d937c713146f6ca7d5290855
Anuj Goel <agoel@Anujs-Air.broadband>
2026-05-31 23:16:17 +0100

    Add video ID extraction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [videoUrl, setVideoUrl] = useState("");
+  const [videoId, setVideoId] = useState("");
+
+  const handleCheckRating = () => {
+  const extractedVideoId =
+    new URL(videoUrl).searchParams.get("v") || "";
+
+  setVideoId(extractedVideoId);
+  };
+  
+
   return (
     <main className="min-h-screen bg-white px-6 flex items-center justify-center">
       <section className="w-full max-w-4xl text-center">
@@ -19,10 +34,15 @@ export default function Home() {
           <input
             type="text"
             placeholder="Paste a video URL"
+            value={videoUrl}
+            onChange={(e) => setVideoUrl(e.target.value)}
             className="flex-1 rounded-2xl border border-gray-300 px-5 py-4 text-base outline-none focus:ring-2 focus:ring-gray-300"
           />
 
-          <button className="rounded-2xl bg-gray-900 px-8 py-4 text-white font-medium hover:opacity-90 transition">
+          <button
+            onClick={handleCheckRating}
+            className="rounded-2xl bg-gray-900 px-8 py-4 text-white font-medium hover:opacity-90 transition"
+          >
             Check Rating
           </button>
         </div>
@@ -30,6 +50,11 @@ export default function Home() {
         <p className="text-sm text-gray-400 mt-5">
           Currently supports YouTube videos
         </p>
+        {videoId && (
+          <p className="mt-6 text-gray-700">
+          Video found: <strong>{videoId}</strong>
+          </p>
+        )}
       </section>
     </main>
   );
